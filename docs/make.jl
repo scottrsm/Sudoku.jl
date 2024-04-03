20ce9594ca7f52c4e91892bb3c7092296a55573c
Scott McIntire <scottrsm@gmail.com>
2024-04-03 16:39:39 -0400

    Added github doc and test actions.

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,18 @@
-using Documenter, Sudoku
+using Sudoku
+import Pkg
 
-makedocs(sitename="Sudoku Docs")
+Pkg.add("Documenter")
+using Documenter
 
+makedocs(
+	sitename = "Sudoku",
+	format = Documenter.HTML(),
+	modules = [Sudoku]
+	)
+
+	# Documenter can also automatically deploy documentation to gh-pages.
+	# See "Hosting Documentation" and deploydocs() in the Documenter manual
+	# for more information.
+	deploydocs(
+		repo = "github.com/scottrsm/Sudoku.jl.git"
+	)
